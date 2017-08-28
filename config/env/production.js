0dd43a7d9a0ac46ae2d7d966512e9fa07f668807
Claysllan Xavier <claysllan-xavier@hotmail.com>
2017-08-28 19:25:28 -0300

    Configuração do banco de dados

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: 'test',
-  db: 'mongodb://172.18.0.2/apieja',
+  db: 'mongodb://1e1f67f26fbfd98ef5ba797c7c2784b6:eja123@5a.mongo.evennode.com:27017/1e1f67f26fbfd98ef5ba797c7c2784b6',
   port: process.env.PORT,
   address: '0.0.0.0',
-  domain: 'localhost'
+  domain: 'apieja.us-3.evennode.com'
 };
