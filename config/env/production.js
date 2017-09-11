df4ec84290e1dcb882c4a15020e2a81444936c7b
Claysllan Xavier <claysllan-xavier@hotmail.com>
2017-09-11 12:12:13 -0300

    Migrando para servidor Azure

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 module.exports = {
-  env: 'test',
-  db: 'mongodb://1e1f67f26fbfd98ef5ba797c7c2784b6:eja123@5a.mongo.evennode.com:27017/1e1f67f26fbfd98ef5ba797c7c2784b6',
+  env: 'production',
+  db: 'mongodb://1e1f67f26fbfd98ef5ba797c7c2784b6:eja123@ds036967.mlab.com:36967/apieja',
   port: process.env.PORT,
   address: '0.0.0.0',
-  domain: 'apieja.us-3.evennode.com'
+  domain: 'http://apieja.azurewebsites.net'
 };
